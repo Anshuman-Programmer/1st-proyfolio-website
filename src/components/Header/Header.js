eafdd7f0019282381cc5b49a7f2127f7054b1b56
Anshuman <anshumantalukdar02@gmail.com>
2021-07-17 22:03:00 +0545

    Header done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+      <h3 className="header__logo">Anshuman T.</h3>
+      <ul className="header__links">
+        <li className="header__link" href="#hero"><span>1.</span>Hero</li>
+        <li className="header__link" href="#about"><span>2.</span>About_me</li>
+        <li className="header__link" href="#projects"><span>3.</span>Projects</li>
+        <li className="header__link" href="#contact"><span>4.</span>Contact_me</li>
+        <li className="header_resume_link"><a>Resume</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
